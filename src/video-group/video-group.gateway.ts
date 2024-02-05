d4aa49ef54e48873b4460dc40a47596a79391dd8
Thaimeo03 <thaitran15072003@gmail.com>
2024-02-05 22:16:58 +0700

    Logic socket video room

--- src/video-group/video-group.gateway.ts
+++ src/video-group/video-group.gateway.ts
@@ -20,23 +20,37 @@ export class VideoGroupGateway implements OnGatewayInit, OnGatewayConnection, On
   // constructor(private readonly ) {}
 
   @SubscribeMessage('join-room')
-  handleJoinRoom(client: Socket, payload: { roomId: string; myId: string }) {
-    const { roomId, myId } = payload
-    console.log(`a new user ${myId} joined room ${roomId}`)
+  handleJoinRoom(client: Socket, payload: { roomId: string; myPeerId: string }) {
+    const { roomId, myPeerId } = payload
+    console.log(`a new user ${myPeerId} joined room ${roomId}`)
 
     // Handle join room
     // ...
     // End handle join room
 
     client.join(roomId)
-    client.broadcast.to(roomId).emit('user-connected', myId)
+    client.broadcast.to(roomId).emit('user-connected', myPeerId)
+  }
+
+  @SubscribeMessage('user-toggle-audio')
+  handleToggleAudio(client: Socket, payload: { roomId: string; myPeerId: string }) {
+    const { roomId, myPeerId } = payload
+    client.join(roomId)
+    client.broadcast.to(roomId).emit('user-toggle-audio', myPeerId)
+  }
+
+  @SubscribeMessage('user-toggle-video')
+  handleToggleVideo(client: Socket, payload: { roomId: string; myPeerId: string }) {
+    const { roomId, myPeerId } = payload
+    client.join(roomId)
+    client.broadcast.to(roomId).emit('user-toggle-video', myPeerId)
   }
 
   @SubscribeMessage('user-leave')
-  handleLeaveRoom(client: Socket, payload: { roomId: string; myId: string }) {
-    const { roomId, myId } = payload
+  handleLeaveRoom(client: Socket, payload: { roomId: string; myPeerId: string }) {
+    const { roomId, myPeerId } = payload
     client.join(roomId)
-    client.broadcast.to(roomId).emit('user-leave', myId)
+    client.broadcast.to(roomId).emit('user-leave', myPeerId)
   }
 
   afterInit(server: Server) {

c4448a63ea63a79669fdf1b33a938cf813661589
Thaimeo03 <thaitran15072003@gmail.com>
2024-03-12 10:58:12 +0700

    Set cookie user role

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common'
+import { Controller, Get, Param, Req, Res, UseGuards } from '@nestjs/common'
 import { RolesService } from './roles.service'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
-import { Request } from 'express'
+import { Request, Response } from 'express'
 import { IJwtPayload } from 'common/strategies/jwt.strategy'
 import { ResponseData } from 'common/customs/response-data'
 
@@ -11,9 +11,15 @@ export class RolesController {
 
   @Get(':room_id')
   @UseGuards(JwtAuthGuard)
-  async getRole(@Param('room_id') room_id: string, @Req() req: Request) {
+  async getRole(@Param('room_id') room_id: string, @Req() req: Request, @Res({ passthrough: true }) res: Response) {
     const user = req.user as IJwtPayload
     const role = await this.rolesService.findRoleByRoomIdAndUserId({ roomId: room_id, userId: user.id })
+
+    const key = JSON.stringify({ roomId: room_id, userId: user.id })
+    if (!req.cookies[key]) {
+      res.cookie(key, role)
+    }
+
     return new ResponseData({
       message: 'Get role success',
       data: role

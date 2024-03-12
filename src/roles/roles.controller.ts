b6de637fbbd6523a8850e4fa70e976c828210c81
Thaimeo03 <thaitran15072003@gmail.com>
2024-03-12 14:14:45 +0700

    Update roles guard

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, Param, Put, Req, UseGuards, Res } from '@nestjs/common'
+import { Controller, Get, Param, Put, Req, UseGuards } from '@nestjs/common'
 import { RolesService } from './roles.service'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
-import { Request, Response } from 'express'
+import { Request } from 'express'
 import { IJwtPayload } from 'common/strategies/jwt.strategy'
 import { ResponseData } from 'common/customs/response-data'
-import { RolesGuard } from 'src/auth/role.guard'
+import { RolesGuard } from 'src/roles/role.guard'
 import { Roles } from 'common/decorators/roles.decorator'
 import { ROLE } from 'common/enums/users.enum'
 
@@ -14,15 +14,10 @@ export class RolesController {
 
   @Get(':room_id')
   @UseGuards(JwtAuthGuard)
-  async getRole(@Param('room_id') room_id: string, @Req() req: Request, @Res({ passthrough: true }) res: Response) {
+  async getRole(@Param('room_id') room_id: string, @Req() req: Request) {
     const user = req.user as IJwtPayload
 
     const role = await this.rolesService.findRoleByRoomIdAndUserId({ roomId: room_id, userId: user.id })
-
-    const key = JSON.stringify({ roomId: room_id, userId: user.id })
-    if (!req.cookies[key]) {
-      res.cookie(key, role)
-    }
 
     return new ResponseData({
       message: 'Get role success',
